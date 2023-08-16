f64959561b2d03890519025446bf4229fff46cd6
francopagnotta <francoapagnotta@gmail.com>
2023-08-16 18:20:31 -0300

    change(movie service): map  data to return from api

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Movie } from '../interfaces/Movie.interface';
+import { MoviesApiResponse } from '../interfaces/MoviesApiResponse.interface';
 
 
 @Injectable({
@@ -11,11 +14,18 @@ export class MovieService {
 
   constructor(private _httpClient: HttpClient) { }
 
-  getTopRatedMovies(): Observable<Object> {
-    return this._httpClient.get(`${environment.baseUrl}/movie/popular`, {
+  getTopRatedMovies(pageNumber?: number): Observable<Movie[]> {
+    return this._httpClient.get<MoviesApiResponse>(`${environment.baseUrl}/movie/popular`, {
       params: {
-        api_key: environment.apiKey
+        api_key: environment.apiKey,
+        page: pageNumber ?? 1
       }
-    });
+    }).pipe(
+      map(response => response.results)
+    )
+  }
+
+  getMovieDetails(id: string) {
+    return this._httpClient.get(`${environment.baseUrl}/movie/${id}`);
   }
 }
