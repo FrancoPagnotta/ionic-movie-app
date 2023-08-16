29507ef2c1acca9ad2728aad26fc819a03aa5759
francopagnotta <francoapagnotta@gmail.com>
2023-08-16 16:50:18 -0300

    add: add routes for movies and movie components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'movies',
+    loadComponent: () => import('./movies/movies.component').then((m) => m.MoviesComponent),
+  },
+  {
+    path: 'movies/:id',
+    loadComponent: () => import('./movie/movie.component').then((m) => m.MovieComponent),
   },
 ];
