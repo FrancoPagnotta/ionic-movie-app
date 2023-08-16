b6702d21fd990793450448974d969e1ab200558a
francopagnotta <francoapagnotta@gmail.com>
2023-08-16 17:54:07 -0300

    add(movie service): add method to get popular movies

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -8,4 +10,12 @@ import { HttpClient } from '@angular/common/http';
 export class MovieService {
 
   constructor(private _httpClient: HttpClient) { }
+
+  getTopRatedMovies(): Observable<Object> {
+    return this._httpClient.get(`${environment.baseUrl}/movie/popular`, {
+      params: {
+        api_key: environment.apiKey
+      }
+    });
+  }
 }
